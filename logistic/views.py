91064176d3efe35211268947e65683379f51a103
Lex <alecsaid@yandex.ru>
2024-07-18 10:11:32 +1100

    Initial commit

---
+++ logistic/views.py
@@ -0,0 +1,30 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
+from rest_framework.pagination import PageNumberPagination
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+class ProductPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class StockPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = ProductPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    pagination_class = StockPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['address', 'products__title']
